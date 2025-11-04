c6b9dcb1f10dde14f178e9770a7fc43560995437
Adam Soltys <adam@coinos.io>
2025-11-04 09:45:00 -0800

    coinos settings

--- cashu/core/settings.py
+++ cashu/core/settings.py
@@ -285,6 +285,8 @@ class CLNRestFundingSource(MintSettings):
     mint_clnrest_cert: Optional[str] = Field(default=None)
     mint_clnrest_rune: Optional[str] = Field(default=None)
     mint_clnrest_enable_mpp: bool = Field(default=True)
+    mint_clnrest_coinos_url: Optional[str] = Field(default=False)
+    mint_clnrest_coinos_jwt: Optional[str] = Field(default=False)
 
 
 class CoreLightningRestFundingSource(MintSettings):
